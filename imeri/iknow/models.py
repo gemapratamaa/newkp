c2f0c1e199f475ef9d6d8414088d29277b8fa712
Gema Pratama Aditya <gemapratamaaditya@yahoo.com>
2020-07-23 14:14:32 +0700

    updated model

--- imeri/iknow/models.py
+++ imeri/iknow/models.py
@@ -1,20 +1,25 @@
 from django.db import models
-from django_mysql.models import ListCharField
+# from django_mysql.models import ListCharField
 
-# Create your models here.
-class Person(models.Model):
-    title = models.CharField(max_length=300)
+class Publication(models.Model):
+	title = models.CharField(max_length=300)
 	
 	"""
 	authors = ListCharField(
-        base_field=CharField(max_length=100),
-        size=6,
-        max_length=(6 * 11)  # 6 * 10 character nominals, plus commas
-    )
+		base_field=CharField(max_length=100),
+		size=6,
+		max_length=(6 * 11)  # 6 * 10 character nominals, plus commas
+	)
 	"""
 	authors = models.CharField(max_length=500)
 	abstract = models.CharField(max_length=50000)
 	publisher = models.CharField(max_length=500)
-	volume = models.IntegerField
-
-    last_name = models.CharField(max_length=30)
+	volume = models.IntegerField()
+	issue_no = models.CharField(max_length=500)
+	pages = models.CharField(max_length=500)
+	issue_date = models.DateField()
+	doi = models.CharField(max_length=500)
+	uri = models.CharField(max_length=500)
+	issn = models.CharField(max_length=500)
+	keywords = models.CharField(max_length=500)
+	type_of = models.CharField(max_length=500)
