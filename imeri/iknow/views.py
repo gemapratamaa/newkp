63212194607b2427cf5c799fbd3b48c4c7e50b3c
Gema Pratama Aditya <gemapratamaaditya@yahoo.com>
2020-07-23 15:39:58 +0700

    attempt upload metadata

--- imeri/iknow/views.py
+++ imeri/iknow/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from imeri.iknow.models import Publication
 
 import csv
 from io import StringIO
@@ -6,10 +7,8 @@ from io import StringIO
 def home(request):
 	return render(request, 'home.html')
 
-	
 def handle_imported_csv(request):
 	pass
-
 
 def import_metadata(request):
 	if request.method == 'POST':# and request.FILES['metadata']:
@@ -17,29 +16,30 @@ def import_metadata(request):
 
 		my_file = request.FILES['metadata']
 		
-		print("my file: ", str(my_file))
+		#print("my file: ", str(my_file))
 		file = my_file.read().decode('utf-8')
 		csv_data = csv.reader(StringIO(file), delimiter=',')
 		for row in csv_data:
 			for i, field in enumerate(row):
-				print(i, field)
-				title = row[2]
-				authors = ", ".join(row[3].split("||"))
-				abstract = row[4]
-				publisher = row[5]
-				type_of = row[11]
-				
-				volume = models.IntegerField()
-				issue_no = models.CharField(max_length=500)
-				pages = models.CharField(max_length=500)
-				issue_date = models.DateField()
-				doi = models.CharField(max_length=500)
-				uri = models.CharField(max_length=500)
-				issn = models.CharField(max_length=500)
-				keywords = models.CharField(max_length=500)
-				type_of = models.CharField(max_length=500)
-				video = models.CharField(max_length=500)
-			print("========================")
+				publication = Publication(
+					title=row[2],
+					authors=", ".join(row[3].split("||")),
+					abstract= row[4],
+					publisher= row[5],
+					date_issued= row[6],
+					doi=row[7],
+					issn=row[8],
+					keywords=", ".join(row[9].split("||")),
+					language=row[10],
+					type_of=row[11],
+					volume=row[12],
+					issue_no=row[13],
+					pages=row[14],
+					uri=row[15],
+					video=row[16]
+				)
+
+				publication.save()
 		
 		return render(request, 'import_metadata.html')
 	
